2bc9c6349394afb6f5a9806bec167f19d9b383cd
Volodymyr-Bezyk <99117313+Volodymyr-Bezyk@users.noreply.github.com>
2023-08-21 13:07:26 +0300

    feature/services section

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3-alpha-sig.figma.com',
+        port: '',
+        pathname: '/img/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 
+import Img01 from '@/../public/services/01.jpg';
 import Container from '@/components/Container';
 
 const Home = () => (
@@ -37,7 +38,7 @@ const Home = () => (
                 </li>
                 <li>
                   <a
-                    href="/"
+                    href="#services"
                     rel="noopener noreferrer nofollow"
                     className=" headerNavLink"
                   >
@@ -46,7 +47,7 @@ const Home = () => (
                 </li>
                 <li>
                   <a
-                    href="/"
+                    href="#career"
                     rel="noopener noreferrer nofollow"
                     className=" headerNavLink"
                   >
@@ -55,7 +56,7 @@ const Home = () => (
                 </li>
                 <li>
                   <a
-                    href="/"
+                    href="#gallery"
                     rel="noopener noreferrer nofollow"
                     className=" headerNavLink"
                   >
@@ -64,7 +65,7 @@ const Home = () => (
                 </li>
                 <li>
                   <a
-                    href="/"
+                    href="#footer"
                     rel="noopener noreferrer nofollow"
                     className=" headerNavLink"
                   >
@@ -172,6 +173,59 @@ const Home = () => (
           knowledge of the Carpathian landscapes lead you safely through the
           mysteries of these mountains.
         </p>
+      </Container>
+    </section>
+
+    {/* ******SERVICES SECTION */ }
+    <section
+      className=" py-14 services responsive tablet:py-16 desktop:py-104px"
+      id="services"
+    >
+      <Container>
+        <div className='tablet:flex tablet:justify-between tablet:mb-9'>
+          <h2 className=" mb-6  uppercase text-40 font-thin leading-snug -tracking-1.6 tablet:mb-0  tablet:text-67 tablet:-tracking-2.68  desktop:text-98 desktop:-tracking-3.92">
+            WE <strong className=" font-medium">OFFER</strong>
+          </h2>
+          <p className=" mb-4 text-43 font-thin leading-normal text-right tablet:pr-14 tablet:text-67 tablet:leading-tight desktop:pr-368px desktop:text-98 desktop:leading-normal">
+            <span>01</span>/<span className=" text-secondaryText">05</span>
+          </p>
+        </div>
+
+        <div className=' tablet:flex tablet:justify-between'>
+          <Image
+            src={ Img01 }
+            alt="mountains enternainment"
+            width={ 460 }
+            height={ 250 }
+            className=' tablet:w-463px desktop:w-606px'
+          />
+          <div className='tablet:w-220px tablet:flex tablet:flex-col desktop:w-606px desktop:h-484px desktop:flex-wrap'>
+            <p className="opacity-50 mt-3 mb-6 text-right text-xs font-extralight leading-6 tracking-2.4 tablet:mt-0 tablet:mb-8 tablet:text-left desktop:w-294px desktop:mb-auto">
+              Feel the adrenaline rush
+            </p>
+
+            <ul className='grid gap-4 w-44 mb-9 tablet:order-first tablet:mb-6 desktop:w-64 desktop:gap-6 desktop:mb-230px '>
+              <li>
+                <button className='activeService uppercase text-left opacity-50 text-xl font-extralight leading-0.85 tablet:text-22px desktop:text-28 desktop:leading-6 ' type='button' >ATVs Traveling</button>
+              </li>
+              <li>
+                <button className=' uppercase text-left opacity-50 text-xl font-extralight leading-0.85 tablet:text-22px desktop:text-28 desktop:leading-6' type='button'>Rock climbing</button>
+              </li>
+              <li>
+                <button className=' uppercase text-left opacity-50 text-xl font-extralight leading-0.85 tablet:text-22px desktop:text-28 desktop:leading-6' type='button'>Hot air ballooning</button>
+              </li>
+              <li>
+                <button className=' uppercase text-left opacity-50 text-xl font-extralight leading-0.85 tablet:text-22px desktop:text-28 desktop:leading-6' type='button'>Skydiving</button>
+              </li>
+              <li>
+                <button className=' uppercase text-left opacity-50 text-xl font-extralight leading-0.85 tablet:text-22px desktop:text-28 desktop:leading-6' type='button'>Rafting</button>
+              </li>
+            </ul>
+
+            <p className=' opacity-50 text-sm font-extralight leading-5 tablet:text-13 tablet:text-justify desktop:w-294px desktop:text-lg desktop:leading-6'>Join exciting rafting expeditions on the waterways of the Carpathians. Go through challenging waterways and overcome gusty waves, feel the adrenaline, and enjoy the incredible views of the surrounding mountains.</p>
+
+          </div>
+        </div>
       </Container>
     </section>
 
@@ -307,7 +361,7 @@ const Home = () => (
             </label>
 
             <button
-              className=" ml-auto block w-20 uppercase text-3xl font-normal leading-normal hover:text-mainBtnBgHover transition-colors duration-300 tablet:order-2 desktop:text-32"
+              className=" ml-auto block w-20 uppercase text-3xl font-normal leading-normal hover:text-mainBtnBgHover transition-colors duration-300 tablet:order-2 tablet:w-auto desktop:text-32"
               type="submit"
               aria-label="Submit form button"
             >
@@ -318,47 +372,179 @@ const Home = () => (
       </Container>
     </section>
 
-    <footer className="  responsive footer py-14 tablet:py-16 desktop:py-104px ">
+    {/* *****GALLERY */ }
+    <section
+      className="responsive gallery py-14 tablet:py-16 desktop:py-104px"
+      id="gallery"
+    >
       <Container>
-        <h2 className=' mb-9 uppercase text-40 font-thin leading-snug -tracking-1.6 tablet:mb-9 tablet:text-67 tablet:leading-none tablet:-tracking-2.68 desktop:mb-71px desktop:text-98 desktop:leading-normal desktop:tracking-3.92'>Contact <strong className=" font-medium">US</strong></h2>
+        <h2 className=" mb-6 uppercase text-40 font-thin leading-snug -tracking-1.6 tablet:mb-72px tablet:text-center tablet:text-67 tablet:leading-tight tablet:-tracking-2.68 desktop:mb-6 desktop:text-98 desktop:leading-normal desktop:tracking-3.92 desktop:text-left">
+          OUR <strong className=" font-medium">GALLERY</strong>
+        </h2>
+        <ul className=" grid place-items-center gap-6 tablet:grid-flow-col ">
+          <li className=" relative  tablet:h-full tablet:col-span-1 tablet:flex tablet:flex-col tablet:justify-center ">
+            <div className="relative galleryLayer">
+              <Image
+                width={ 460 }
+                height={ 250 }
+                src="https://s3-alpha-sig.figma.com/img/6df7/220e/14fc7939fe2cc296422c5658790ca536?Expires=1693785600&Signature=PVr9OeKF74mUIPZSyIB7q-SzKXaRCWjPaI4AC0XKTQeaJJTLv9Vb4TIA1W4fYIDcKFOHW6aPfiZwlCSnogJ5RDZ3ljF2D0WLgGEjGJzU2NOGm9tsD-dxMALU6XR6haZoge1ukuCVnReSVjArUabOX9Yd3uSsKxsD7oJII94gImU5F4CzqdizDFJ3HXTaTc9bBvmxiXz0YHskt0olKxJUFgIcAj0QcmdS69HyrJdLhrBpJc0hmgbTsqbhbzGWgyxiU8WgWMwl6dGCI9W6Fm1BRO4r1OwlC2lEIJEp5B3coNNgeSBmkCYPx~mEx6sPkQfh1jfjVgM3f3zAoPI~FAZ~lQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                alt="Mountains"
+                className="tablet:w-120px desktop:w-300px"
+              />
+            </div>
 
-        <div className=' desktop:flex desktop:items-start desktop:justify-between'>
-          <div className=' tablet:flex tablet:flex-col tablet:flex-wrap tablet:h-104px tablet:items-end tablet:content-around tablet:mb-16 desktop:flex-nowrap'>
-            <div className=' mb-6 flex justify-end items-baseline desktop:mb-16 '>
-              <div className=' mr-5'>
-                <a className=' block text-sm leading-6 tablet:text-base desktop:text-lg desktop:leading-6' href='tel:+380981234567' rel='noopener noreferrer nofollow' target='_blank' aria-label='phone call'>+38 (098) 12 34 567</a>
-                <a className=' block text-sm leading-6 tablet:text-base desktop:text-lg desktop:leading-6' href='tel:+380731234567' rel='noopener noreferrer nofollow' target='_blank' aria-label='phone call'>+38 (073) 12 34 567</a>
+            <button
+              className=" hidden absolute bottom-4 left-0  w-full text-right text-33 font-thin leading-normal tablet:block desktop:bottom-0"
+              type="button"
+              aria-label="Previous image"
+            >
+              BACK{ ' ' }
+            </button>
+          </li>
+
+          <li className=" tablet:col-span-1">
+            <div>
+              <Image
+                width={ 460 }
+                height={ 250 }
+                src="https://s3-alpha-sig.figma.com/img/e0c0/1047/09e209ca76e04687e2606f05fade702e?Expires=1693785600&Signature=KMiG6lbRDfNzeIhuyQwHdxQv5SGNjy37lSqAt0zwG7le9umiayXcssDwC05ZpfWdJtWjxhmGwylLniZ-VaV2ckaBvzQMVRC4LUQyo30bc-2GrUfAWB-6bV~5WPSD6rqwLD6CyOijsNQ7EcMz5uDvZ~LIKD2n-sYEogkJJd6xpJ-RD9YsyuwoZYGkXDJwsKPN55FGTpVv-LfT6Oy1eNOv82p33TG61kT8fK~dTWj7ldIQ0P75427jQgEBA2K~D2Rs2esnE7dBfL8A7JXDTAuBKMN579l-ax9bNy40cSAZd4wZ8QZ9KmlSbuY5FXSyCO5pDEsl53vi1hqZGXKd~aXJXg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                alt="Mountains"
+                className=" desktop:w-606px desktop:h-436px"
+              />
+            </div>
+          </li>
+
+          <li className=" relative  tablet:h-full tablet:col-span-1 tablet:flex tablet:flex-col tablet:justify-center ">
+            <div className="relative galleryLayer">
+              <Image
+                width={ 460 }
+                height={ 250 }
+                src="https://s3-alpha-sig.figma.com/img/32ac/5b43/c0109965c0d25370810e123963404844?Expires=1693785600&Signature=XDeG13y04CdJ5tUNmggMtV7vmz9N9VwIHnMoSAfjsRmvIkbwR~meTDSRl~4JTwgPhaSUhGfRjhpSI2~Mxn5pd~kzT4CVQQlvRS2ezU8~F7KF2hS5DSsnF-eFrKr3mfAU4PEiQWK1ELTF-bHujVy~faJKT2oc0nrPTopvoCu9uERefwgrJIvrjT9KNmyyhcJI3qnknHArxMCZsLiAqrH3DgLwa2NCrJz0HRIPkupzUPXWb1rq9Y2a~Zlx5rnYCVuQmUvinyKwOJ0EY528YluDdOEziSF2VLKDzAYfGr0rzVMpgFQaH4Ih5hQXahf5rui~~FFc22Lg6BXKzXAicIxwjw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                alt="Mountains"
+                className=" tablet:w-120px desktop:w-300px"
+              />
+            </div>
+            <button
+              className="hidden absolute bottom-4 left-0 w-full text-left text-33 font-thin leading-normal tablet:block desktop:bottom-0"
+              type="button"
+              aria-label="Next image"
+            >
+              { ' ' }
+              NEXT
+            </button>
+          </li>
+        </ul>
+      </Container>
+    </section>
+
+    {/* *********FOOTER */ }
+    <footer
+      className="  responsive footer py-14 tablet:py-16 desktop:py-104px "
+      id="footer"
+    >
+      <Container>
+        <h2 className=" mb-9 uppercase text-40 font-thin leading-snug -tracking-1.6 tablet:mb-9 tablet:text-67 tablet:leading-none tablet:-tracking-2.68 desktop:mb-71px desktop:text-98 desktop:leading-normal desktop:tracking-3.92">
+          Contact <strong className=" font-medium">US</strong>
+        </h2>
+
+        <div className=" desktop:flex desktop:items-start desktop:justify-between">
+          <div className=" tablet:flex tablet:flex-col tablet:flex-wrap tablet:h-104px tablet:items-end tablet:content-around tablet:mb-16 desktop:flex-nowrap">
+            <div className=" mb-6 flex justify-end items-baseline desktop:mb-16 ">
+              <div className=" mr-5">
+                <a
+                  className=" block text-sm leading-6 tablet:text-base desktop:text-lg desktop:leading-6"
+                  href="tel:+380981234567"
+                  rel="noopener noreferrer nofollow"
+                  target="_blank"
+                  aria-label="phone call"
+                >
+                  +38 (098) 12 34 567
+                </a>
+                <a
+                  className=" block text-sm leading-6 tablet:text-base desktop:text-lg desktop:leading-6"
+                  href="tel:+380731234567"
+                  rel="noopener noreferrer nofollow"
+                  target="_blank"
+                  aria-label="phone call"
+                >
+                  +38 (073) 12 34 567
+                </a>
               </div>
-              <p className=' w-86px text-xs font-extralight leading-5 desktop:leading-6'>Phone number</p>
+              <p className=" w-86px text-xs font-extralight leading-5 desktop:leading-6">
+                Phone number
+              </p>
             </div>
 
-            <div className=' mb-6 flex justify-end items-baseline tablet:mb-0 desktop:mb-32'>
-              <a className='block text-sm leading-6 tablet:text-base desktop:text-lg desktop:leading-6' href="mailto:support@carptravel.com" rel='noopener noreferrer nofollow' target='_blank' aria-label='write email'>support@carptravel.com</a>
-              <p className=' w-86px ml-5 text-xs font-extralight leading-5 desktop:leading-6'>E-mail</p>
+            <div className=" mb-6 flex justify-end items-baseline tablet:mb-0 desktop:mb-32">
+              <a
+                className="block text-sm leading-6 tablet:text-base desktop:text-lg desktop:leading-6"
+                href="mailto:support@carptravel.com"
+                rel="noopener noreferrer nofollow"
+                target="_blank"
+                aria-label="write email"
+              >
+                support@carptravel.com
+              </a>
+              <p className=" w-86px ml-5 text-xs font-extralight leading-5 desktop:leading-6">
+                E-mail
+              </p>
             </div>
 
-            <div className=' mb-3 flex justify-end items-baseline tablet:mb-0 tablet:ml-90px'>
-              <p className=' mr-5  text-xs font-extralight leading-5 desktop:leading-6 desktop:order-last desktop:w-86px desktop:ml-5 desktop:mr-0'>Follow us</p>
+            <div className=" mb-3 flex justify-end items-baseline tablet:mb-0 tablet:ml-90px">
+              <p className=" mr-5  text-xs font-extralight leading-5 desktop:leading-6 desktop:order-last desktop:w-86px desktop:ml-5 desktop:mr-0">
+                Follow us
+              </p>
               <ul>
-                <li className='w-86px desktop:text-right'>
-                  <a className='footerSocialLink tablet:text-base desktop:text-lg desktop:leading-6  ' href="/" rel='noopener noreferrer nofollow' target='_blank' aria-label='likt to socials'>facebook</a>
+                <li className="w-86px desktop:text-right">
+                  <a
+                    className="footerSocialLink tablet:text-base desktop:text-lg desktop:leading-6  "
+                    href="/"
+                    rel="noopener noreferrer nofollow"
+                    target="_blank"
+                    aria-label="likt to socials"
+                  >
+                    facebook
+                  </a>
                 </li>
-                <li className='w-86px desktop:text-right'>
-                  <a className='footerSocialLink tablet:text-base desktop:text-lg desktop:leading-6' href="/" rel='noopener noreferrer nofollow' target='_blank' aria-label='likt to socials'>instagram</a>
+                <li className="w-86px desktop:text-right">
+                  <a
+                    className="footerSocialLink tablet:text-base desktop:text-lg desktop:leading-6"
+                    href="/"
+                    rel="noopener noreferrer nofollow"
+                    target="_blank"
+                    aria-label="likt to socials"
+                  >
+                    instagram
+                  </a>
                 </li>
-                <li className='w-86px desktop:text-right'>
-                  <a className='footerSocialLink tablet:text-base desktop:text-lg desktop:leading-6' href="/" rel='noopener noreferrer nofollow' target='_blank' aria-label='likt to socials'>youtube</a>
+                <li className="w-86px desktop:text-right">
+                  <a
+                    className="footerSocialLink tablet:text-base desktop:text-lg desktop:leading-6"
+                    href="/"
+                    rel="noopener noreferrer nofollow"
+                    target="_blank"
+                    aria-label="likt to socials"
+                  >
+                    youtube
+                  </a>
                 </li>
-                <li className='w-86px desktop:text-right'>
-                  <a className='footerSocialLink tablet:text-base desktop:text-lg desktop:leading-6' href="/" rel='noopener noreferrer nofollow' target='_blank' aria-label='likt to socials'>tiktok</a>
+                <li className="w-86px desktop:text-right">
+                  <a
+                    className="footerSocialLink tablet:text-base desktop:text-lg desktop:leading-6"
+                    href="/"
+                    rel="noopener noreferrer nofollow"
+                    target="_blank"
+                    aria-label="likt to socials"
+                  >
+                    tiktok
+                  </a>
                 </li>
               </ul>
             </div>
           </div>
 
-          <form className='tablet:flex tablet:flex-col tablet:flex-wrap tablet:h-305px desktop:flex-row desktop:w-606px '>
-
-            <label className=" relative mb-6 block tablet:mb-7 tablet:w-56 ">
+          <form className="tablet:flex tablet:flex-col tablet:flex-wrap tablet:h-305px desktop:flex-row desktop:h-auto desktop:w-606px ">
+            <label className=" relative mb-6 block tablet:mb-7 tablet:w-56 desktop:w-294px desktop:mb-0 ">
               <span className=" mb-1 block text-xs font-extralight leading-6 tracking-2.4">
                 Full name
               </span>
@@ -372,7 +558,7 @@ const Home = () => (
               </span>
             </label>
 
-            <label className=" relative mb-6 block tablet:w-56 ">
+            <label className=" relative mb-6 block tablet:w-56 desktop:w-294px desktop:mb-0 desktop:ml-auto ">
               <span className=" mb-1 block text-xs font-extralight leading-6 tracking-2.4">
                 E-mail
               </span>
@@ -386,15 +572,15 @@ const Home = () => (
               </span>
             </label>
 
-            <label className=" mb-4 block tablet:ml-5 tablet:w-463px tablet:h-244px desktop:ml-0 desktop:w-606px   ">
+            <label className=" mb-4 block tablet:ml-5 tablet:w-463px tablet:h-244px desktop:ml-0 desktop:w-606px desktop:mt-10   ">
               <span className=" mb-1 block text-xs font-extralight leading-6 tracking-2.4">
                 Message
               </span>
-              <textarea className=" px-2 h-196px resize-none bg-inputBg w-full text-13 font-extralight leading-6 outline-none tablet:h-220px " />
+              <textarea className=" px-2 h-196px resize-none bg-inputBg w-full text-13 font-extralight leading-6 outline-none tablet:h-220px desktop:text-xl desktop:leading-6 desktop:py-2px" />
             </label>
 
             <button
-              className=" ml-auto block w-20 uppercase text-3xl font-normal leading-normal hover:text-mainBtnBgHover transition-colors duration-300 desktop:text-32"
+              className=" ml-auto block w-20 uppercase text-3xl font-normal leading-normal hover:text-mainBtnBgHover transition-colors duration-300 tablet:w-auto desktop:text-32"
               type="submit"
               aria-label="Submit form button"
             >
