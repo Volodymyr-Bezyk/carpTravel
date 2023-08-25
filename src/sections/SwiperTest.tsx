f53d48096d26db7599dd1c207bd667050ea9ae4a
Volodymyr-Bezyk <99117313+Volodymyr-Bezyk@users.noreply.github.com>
2023-08-25 09:13:47 +0300

    feature/gallery slider

--- src/sections/SwiperTest.tsx
+++ src/sections/SwiperTest.tsx
@@ -1,22 +1,15 @@
 'use client';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { A11y, Navigation, EffectCoverflow } from 'swiper/modules';
 import React from 'react';
 import Image from 'next/image';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import {
-  A11y,
-  Navigation,
-  Pagination,
-  EffectCoverflow,
-  EffectFade,
-} from 'swiper/modules';
 
-import { Section, Container, SectionTitle } from '@/components';
+import { Section, SectionTitle } from '@/components';
 import { galleryImages, galleryImagesLocal } from '@/constants';
 
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
-import 'swiper/css/effect-fade';
-import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 const SwiperTest = () => {
@@ -28,70 +21,73 @@ const SwiperTest = () => {
           strongText="GALLERY"
           customStyles="mb-6 tablet:mb-72px tablet:text-center tablet:leading-tight desktop:mb-6 desktop:leading-normal desktop:text-left px-5 tablet:px-8 desktop:px-[24px]"
         />
-        {/* <Swiper
-          tag="div"
-          className=""
-          loop={true}
-          spaceBetween={20}
-          modules={[EffectCoverflow, Navigation, A11y]}
-          effect={'fade'}
-          coverflowEffect={
-            {
-              // rotate: 90,
-              // stretch: 0,
-              // depth: 100,
-              // modifier: 2.5,
-              // slideShadows: true,
-            }
-          }
-          // slidesPerView={3}
-          centeredSlides={true}
-        > */}
 
         <Swiper
           effect={'coverflow'}
           grabCursor={true}
           centeredSlides={true}
           loop={true}
-          // spaceBetween={20}
+          initialSlide={0}
           slidesPerView={'auto'}
+          slideToClickedSlide={true}
           coverflowEffect={{
             rotate: 0,
             stretch: 0,
             depth: 350,
             scale: 0.55,
-            modifier: 0.8,
+            modifier: window.innerWidth >= 1440 ? 0.8 : 1.32,
           }}
-          pagination={{ el: '.swiper-pagination', clickable: true }}
-          // navigation={{
-          //   nextEl: '.swiper-button-next',
-          //   prevEl: '.swiper-button-prev',
-          // }}
-          modules={[EffectCoverflow, EffectFade, Pagination, Navigation]}
-          className=""
+          navigation={{
+            nextEl: '.nextSlide',
+            prevEl: '.prevSlide',
+          }}
+          modules={[EffectCoverflow, Navigation, A11y]}
+          className=" swiperStyles"
         >
           {galleryImages.map((src, idx) => (
             <SwiperSlide key={idx} className=" gallerySlide">
-              {/* <div className=" relative h-[225px] w-[312px] overflow-hidden "> */}
-              <div className=" w-[606px]  h-[430px]">
+              <div className=" relative w-[415px] h-[294px] desktop:w-[606px] desktop:h-[430px]">
                 <Image
                   src={src}
                   alt={`gallery image ${idx + 1}`}
-                  // width={606}
-                  // height={430}
                   fill
-                  className=" object-cover"
+                  sizes="(max-width: 768px) 50vw, (min-width: 1200px) 70vw, 100vw"
                 />
               </div>
             </SwiperSlide>
           ))}
 
-          {/* <div className="slider-controler">
-            <div className="swiper-button-prev slider-arrow"></div>
-            <div className="swiper-button-next slider-arrow"></div>
-            <div className="swiper-pagination"></div>
-          </div> */}
+          <div className="  hidden tablet:flex justify-between px-16 absolute w-full bottom-[17px] left-0 z-20 desktop:px-56 desktop:bottom-0 ">
+            <button
+              className=" prevSlide opacity-50 text-right text-33 font-thin leading-normal tablet:block hover:opacity-100 transition-opacity duration-300"
+              type="button"
+              aria-label="Previous image"
+            >
+              BACK
+            </button>
+            <button
+              className=" nextSlide opacity-50 text-left text-33 font-thin leading-normal tablet:block hover:opacity-100 transition-opacity duration-300"
+              type="button"
+              aria-label="Next image"
+            >
+              NEXT
+            </button>
+          </div>
         </Swiper>
+
+        <ul className=" grid px-5 gap-6 tablet:grid-flow-col tablet:hidden ">
+          {galleryImagesLocal.map((src, idx) => (
+            <li key={idx}>
+              <Image
+                src={src}
+                width={460}
+                height={250}
+                alt={`gallery image ${idx + 1}`}
+                className="shadow-galleryShadow"
+              />
+            </li>
+          ))}
+        </ul>
       </div>
     </Section>
   );
