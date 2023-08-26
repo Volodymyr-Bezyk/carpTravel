ba637e2a3d22e006c4e9b8b011800019a0dff88f
Volodymyr-Bezyk <99117313+Volodymyr-Bezyk@users.noreply.github.com>
2023-08-26 09:56:02 +0300

    feature/ dynamic import of sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
 'use client';
 import ScrollToTop from 'react-scroll-to-top';
+import dynamic from 'next/dynamic';
 
-import {
-  Header,
-  Hero,
-  About,
-  Services,
-  Career,
-  Gallery,
-  Contacts,
-} from '@/sections';
+// import {
+// Header,
+// Hero,
+// About,
+// Services,
+// Career,
+// Gallery,
+// Contacts,
+// } from '@/sections';
+
+const Header = dynamic(() => import('../sections/Header'));
+const Hero = dynamic(() => import('../sections/Hero'));
+const About = dynamic(() => import('../sections/About'));
+const Services = dynamic(() => import('../sections/Services'));
+const Career = dynamic(() => import('../sections/Career'));
+const Gallery = dynamic(() => import('../sections/Gallery'));
+const Contacts = dynamic(() => import('../sections/Contacts'));
 
 const Home = () => (
   <main className="bg-galleryBg relative">
