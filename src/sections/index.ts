452a90bafe1a550aa519710a7a76676b0dffe93a
Volodymyr-Bezyk <99117313+Volodymyr-Bezyk@users.noreply.github.com>
2023-08-27 15:25:56 +0300

    fix/delete unknown import in types

--- src/sections/index.ts
+++
@@ -1,9 +0,0 @@
-import Header from './Header';
-import Hero from './Hero';
-import About from './About';
-import Services from './Services';
-import Career from './Career';
-import Gallery from './Gallery';
-import Contacts from './Contacts';
-
-export { Header, Hero, About, Services, Career, Gallery, Contacts };
