508c8bf336fbc666ba87bbca08255971b31cd69f
Volodymyr-Bezyk <99117313+Volodymyr-Bezyk@users.noreply.github.com>
2023-08-29 12:53:10 +0300

    fix/ogp img attempt 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'CarpTravel',
     description: 'Uncover secrets of the Carpathian mountains',
-    images: './favicon.ico',
+    images: './ogpLogo.ico',
     url: 'https://carptravel-five.vercel.app/',
   },
 };
